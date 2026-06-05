a6408c5d5e21b5a68f93adeb00eb3e5e879c83c4
kanchan sharma <kanusharma881388@gmail.com>
2026-06-05 21:58:48 +0530

    fix(slack): deprecate socket mode to leverage production grade http signature verification

--- backend/src/config/slack.ts
+++ backend/src/config/slack.ts
@@ -1,10 +1,21 @@
-import { App } from '@slack/bolt';
+// import { App } from '@slack/bolt';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// export const slackApp = new App({
+//   token: process.env.SLACK_BOT_TOKEN as string,
+//   appToken: process.env.SLACK_APP_TOKEN as string,
+//   socketMode: true,
+// });
+
+import pkg from '@slack/bolt';
+const { App } = pkg;
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const slackApp = new App({
   token: process.env.SLACK_BOT_TOKEN as string,
-  appToken: process.env.SLACK_APP_TOKEN as string,
-  socketMode: true,
+  signingSecret: process.env.SLACK_SIGNING_SECRET as string,
 });
