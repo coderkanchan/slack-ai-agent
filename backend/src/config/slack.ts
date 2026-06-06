1e9ffbadb6b99fe5e104d6b53fddffc63208226d
kanchan sharma <kanusharma881388@gmail.com>
2026-06-06 20:45:34 +0530

    initial commit

--- backend/src/config/slack.ts
+++ backend/src/config/slack.ts
@@ -1,14 +1,3 @@
-// import { App } from '@slack/bolt';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// export const slackApp = new App({
-//   token: process.env.SLACK_BOT_TOKEN as string,
-//   appToken: process.env.SLACK_APP_TOKEN as string,
-//   socketMode: true,
-// });
-
 import pkg from '@slack/bolt';
 const { App } = pkg;
 import dotenv from 'dotenv';
@@ -19,3 +8,5 @@ export const slackApp = new App({
   token: process.env.SLACK_BOT_TOKEN as string,
   signingSecret: process.env.SLACK_SIGNING_SECRET as string,
 });
+
+
