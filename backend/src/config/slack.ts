fb278d2f0fbb5a2346ee27ede6c567f1442cdafe
kanchan sharma <kanusharma881388@gmail.com>
2026-06-08 18:18:09 +0530

    fix(slack): re-route signature validation directly into bolt express receiver instance

--- backend/src/config/slack.ts
+++ backend/src/config/slack.ts
@@ -1,12 +1,15 @@
 import pkg from '@slack/bolt';
-const { App } = pkg;
+const { App, ExpressReceiver } = pkg;
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const slackApp = new App({
-  token: process.env.SLACK_BOT_TOKEN as string,
-  signingSecret: process.env.SLACK_SIGNING_SECRET as string,
+export const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET || '',
+  processBeforeResponse: true
 });
 
-
+export const slackApp = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: receiver
+});
